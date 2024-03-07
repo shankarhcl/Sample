c4556db26dac15cd9e435fcbace1da7da833f0d5
shankarhcl <79025091+shankarhcl@users.noreply.github.com>
2024-03-08 04:33:16 +0530

    checking whether automatic build working

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ package com.sample.jenkins;
 
 public class Main {
 public static void main(String[] args) {
-	
 	System.out.println("Hey, This is the class to check Whether Jenkins is working!!");
 }
 }
