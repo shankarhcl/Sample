9bfdfc178cbe62928138cc0776b14794904a98eb
shankarhcl <79025091+shankarhcl@users.noreply.github.com>
2024-03-08 04:18:43 +0530

    main2 deleted

--- Main2.java
+++
@@ -1,8 +0,0 @@
-package com.sample.jenkins;
-
-public class Main2 {
-public static void main(String[] args) {
-	
-	System.out.println("Hey, This is the class on 08Mar2024 branch to check Whether Jenkins is working!!");
-}
-}
